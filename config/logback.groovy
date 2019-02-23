73f55756ece81e416dc9dbad10318d8c273f1fe5
SpardaRus <ale-altair@yandex.ru>
2019-02-23 22:00:04 +0400

    Change logback.groovy(path logs)

--- config/logback.groovy
+++ config/logback.groovy
@@ -4,7 +4,7 @@ import ch.qos.logback.classic.filter.ThresholdFilter
 import static ch.qos.logback.classic.Level.*
 
 String INSTANCE_NAME = "Tool bot"
-String PATH_LOG = "var/logs"
+String PATH_LOG = "logs"
 String FILE_PATTERN = "%d{yyyy-MM-dd}_%i.log.gz"
 String MAX_FILE_SIZE = "100MB"
 String ENCODER_PATTERN = "%date{dd/MM/yy HH:mm:ss.SSS} %level [%thread] %logger{10}: %msg%n"
